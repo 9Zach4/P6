2ffdd9ad207941dd0c82c73664f59ee37468661e
Zach <djessitokoto@gmail.com>
2023-07-06 03:32:00 +0200

    import

--- kasa/src/components/card/Card.jsx
+++ kasa/src/components/card/Card.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
+import './card.scss';
 
-
-function Card (){
+function Card ({id, title, cover}){
+    return (
+        // <Link to={`/accomodation/${id}`}>
+            <div className="card_gallery">
+                <img src={cover} alt={title} />
+                <h3>{title}</h3>
+            </div>
+        </Link>
+    );
     
 }
