2b392c03045644b96cfde76fca73bd746bf956b3
Zach <djessitokoto@gmail.com>
2023-07-06 13:46:10 +0200

    gallerie

--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -3,13 +3,28 @@ import Banner from "../../components/banner/Banner"
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import "./home.scss";
+import Card from "../../components/card/Card";
+import housingData from '../../data/Data.js';
+
 export default function Home() {
     return (
         <div className="home">
             <Header />
             <Banner />
+            <main className="grid_gallery">
+                {housingData.map(Data => {
+                    return (
+                        <Card
+                            key={Data.id}
+                            id={Data.id}
+                            title={Data.title}
+                            cover={Data.cover}
+                        />
+                    )
+                })}
+            </main>
             <Footer />
-          
+
         </div>
     )
 }
