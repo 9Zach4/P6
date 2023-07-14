1d59341c388ff62cfcfa8cbeb7e123282db826c9
Zach <djessitokoto@gmail.com>
2023-07-14 16:02:53 +0200

    refactorisation

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,27 +1,16 @@
 //import 
 import React from "react";
-import Navbar from "./components/navbar/Navbar.jsx";  
-import Banner from "./components/banner/Banner.jsx";
+import Banner from "./layout/banner/Banner.jsx";
 import LocationGrid from "./components/locationGrid/LocationGrid.jsx";
 import "./App.css";
-import Main from "./components/main/Main.jsx";
-import Footer from "./components/footer/Footer.jsx";
-//creation du router  avec la methode createBrowserRouter
-
 
 
 function App() {
   return (
-    
-    <div>
-      <Navbar />
-      <Main>
+    <> 
         <Banner />
         <LocationGrid />
-      </ Main>
-      <Footer />
-    </div>
-
+    </>
   );
 }
 
--- kasa/src/routes/router.jsx
+++ kasa/src/routes/router.jsx
@@ -1,14 +1,18 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Footer from "../components/footer/Footer";
+import Footer from "../layout/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
 import React from "react";
+import Main from "../layout/main/Main";
 
 const  HeaderFooter = () => {
     return (
         <>
         <Navbar />
-        <Outlet />
+        <Main>
+          <Outlet />
+        </Main>
+        
         <Footer />
         </>
     );
