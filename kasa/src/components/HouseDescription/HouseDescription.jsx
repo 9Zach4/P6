22a38de4f29907d60fb0d3a220ca83cf56e781b2
Zach <djessitokoto@gmail.com>
2023-07-19 15:15:37 +0200

    media queerie ok page accomodation

--- kasa/src/components/HouseDescription/HouseDescription.jsx
+++ kasa/src/components/HouseDescription/HouseDescription.jsx
@@ -9,7 +9,7 @@ function houseDescription() {
                     <span>Description</span>
                     <i className="fa-solid fa-chevron-up"></i>
                 </p>
-                <p className="description__content">bla bla bla dgdkzYDFzukydgZKYDGzkudygZKUDYGzkudyZZDZDZDZEDZEDZEDZDDZDEFZZEHLZIFHZGE</p>
+                <p className="description__content"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
   );
 }
